81f630c866654a73cfb2321e4ad8361ba9c2530a
swedev99 <swedevops99@gmail.com>
2023-06-22 16:39:10 +0530

    Jenkins pipeline concepts

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require('express');
 const pino = require('pino');
 const expPino = require('express-pino-logger');
 
-// MongoDB
+// MongoDB12
 var db;
 var usersCollection;
 var ordersCollection;
